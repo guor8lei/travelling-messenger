bf0d111ab9cc69cd093ba23c092adfab4ec62572
Raymond Guo <guor.lei@berkeley.edu>
2018-05-18 17:38:46 -0500

    Add request dependency to package.json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
 
 const PAGE_ACCESS_TOKEN = process.env.FB_MSGR_ACCESS_TOKEN // verify token in heroku config
