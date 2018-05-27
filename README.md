a96fd6ebefbb6cb26a7f27f1c380c16a53110233
Raymond Guo <guor.lei@berkeley.edu>
2018-05-26 23:13:42 -0500

    Finish implementation of help menu, hotel, dining, tourist, and weather recommendations
    Yelp API calls for dining, hotel, tourism
    OpenWeatherMap API calls for weather
    Fully functional help menu

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A Facebook Messenger bot that provides travelling information for you! 
 
+Starter code from https://developers.facebook.com/docs/messenger-platform/getting-started
+
 This app is in development mode and is not open to the public. If you are interested in trying the chatbot out, contact me at guor.lei@berkeley.edu.
--- index.js
+++ index.js
@@ -9,12 +9,16 @@ const
   
 // Set up Dialogflow for small talk
 const apiai = require('apiai');
-const apiaiApp = apiai("c314434cf4a14be28da4fd618cecaab7");
+const APIAI_TOKEN = process.env.APIAI_TOKEN; // token in heroku config
+const apiaiApp = apiai(APIAI_TOKEN);
 
 // Set up Yelp Fusion API
-const YELP_TOKEN = process.env.YELP_TOKEN // page access token in heroku config
+const YELP_TOKEN = process.env.YELP_TOKEN // token in heroku config
 const yelp = require('yelp-fusion');
 const yelpClient = yelp.client(YELP_TOKEN);
+
+// Set up OpenWeatherMap API
+const WEATHER_APIKEY = process.env.WEATHER_APIKEY;
 
 const PAGE_ACCESS_TOKEN = process.env.FB_MSGR_ACCESS_TOKEN // page access token in heroku config
 
@@ -24,44 +28,6 @@ app.listen(process.env.PORT || 1337, () => console.log('Weber the Webhook is lis
 // Set reply to index route
 app.get('/', function (req, res) {
 	res.send("Hello world! I am a Facebook Messenger chatbot created by Raymond Guo.");
-});
-
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-	body.entry.forEach(function(entry) {
-
-	  // Gets the body of the webhook event
-	  let webhook_event = entry.messaging[0];
-	  //console.log(webhook_event);
-
-
-	  // Get the sender PSID
-	  let sender_psid = webhook_event.sender.id;
-	  //console.log('Sender PSID: ' + sender_psid);
-
-	  // Check if the event is a message or postback and
-	  // pass the event to the appropriate handler function
-	  if (webhook_event.message) {
-	    handleMessage(sender_psid, webhook_event.message);        
-	  } else if (webhook_event.postback) {
-	    handlePostback(sender_psid, webhook_event.postback);
-	  }
-	  
-	});
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
 });
 
 // Adds support for GET requests to our webhook
@@ -92,19 +58,77 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+	  body.entry.forEach(function(entry) {
+
+	  // Gets the body of the webhook event
+	  let webhook_event = entry.messaging[0];
+	  //console.log(webhook_event);
+
+	  // Get the sender PSID
+	  let sender_psid = webhook_event.sender.id;
+	  //console.log('Sender PSID: ' + sender_psid);
+
+	  // Check if the event is a message or postback and
+	  // pass the event to the appropriate handler function
+	  if (webhook_event.message) {
+	    handleMessage(sender_psid, webhook_event.message);        
+	  } else if (webhook_event.postback) {
+	    handlePostback(sender_psid, webhook_event.postback);
+	  }
+	  
+	});
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
 
-  let responseMessenger = {
-    "text": `Sorry, I don't know how to respond. Try typing 'help' to get a list of my functionalities.`
-  }
+  let responseMessenger;
 
   // Check if the message contains text
   if (received_message.text) {    
     
     if (received_message.text.toLowerCase() === "help") {
       responseMessenger = {
-        "text": `I'm a Facebook Messenger chatbot created by Raymond. I hope to be a travel advising companion by integrating together APIs that provide weather, hotel, dining, and flight information. For now, let's just have a conversation!`
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "I'm a Facebook Messenger chatbot created by Raymond. I'm a travel advising companion, integrating together APIs that provide weather, hotel, and dining information. Try some of the entries below!",
+          "buttons": [
+			        {
+                "type": "postback",
+                "title": "Pizza in Boston?",
+                "payload": "food",
+              },
+              {
+                "type": "postback",
+                "title": "Hiking in Portland?",
+                "payload": "tourist",
+              },
+              {
+                "type": "postback",
+                "title": "Hotels in Vegas?",
+                "payload": "hotel",
+              }
+            ]
+          }
+        }
       }
       callSendAPI(sender_psid, responseMessenger);  
       
@@ -126,32 +150,9 @@ function handleMessage(sender_psid, received_message) {
     
       dialogflow.end();  
     }
-    
-    // Create the payload for a basic text message
-    // response = {
-    //   "attachment": {
-    //     "type": "template",
-    //     "payload": {
-    //       "template_type": "button",
-    //       "text": `Hey son, I've gotten alittle old and a little hard of hhearing, but I think you said "${received_message.text}". Is that wot u said, brother?`,
-    //       "buttons": [
-			 // {
-    //             "type": "postback",
-    //             "title": "Yes ur amazing!",
-    //             "payload": "yes",
-    //           },
-    //           {
-    //             "type": "postback",
-    //             "title": "No u suk old man!",
-    //             "payload": "no",
-    //           }
-    //         ]
-    //       }
-    //     }
-    //   }
   } else {
-  	responseMessenger = {
-      "text": `Sorry, I don't know how to respond. Try typing 'help' to get a list of my functionalities.`
+    responseMessenger = {
+    "text": "Sorry, I don't know how to respond. Try typing 'help' to get a list of my functionalities."
     }
     callSendAPI(sender_psid, responseMessenger);    
   }
@@ -159,19 +160,94 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-  let response;
+  let responseMessenger;
   
   // Get the payload for the postback
   let payload = received_postback.payload;
+  console.log("payload");
 
   // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "That's great!" }
-  } else if (payload === 'no') {
-    response = { "text": "That's too bad." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+  if (payload === 'food') {
+    yelpClient.search({
+      term:'pizza',
+      location: 'Boston'
+    }).then(response => {
+      let firstB = response.jsonBody.businesses[0];
+      let secB = response.jsonBody.businesses[1];
+      let thirdB = response.jsonBody.businesses[2];
+      let msg = "The top pizza restaurants in the city of Boston are: " + firstB.name + " (" + firstB.price + ", " + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
+                                                                             + secB.name + " (" + secB.price + ", " + secB.rating + " stars with " + secB.review_count + " reviews), and " 
+                                                                             + thirdB.name + " (" + thirdB.price + ", " + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
+      
+      responseMessenger = produceResponseTemplate(firstB, secB, thirdB, msg);
+      callSendAPI(sender_psid, responseMessenger);  
+    }).catch(e => {
+      console.log(e);
+    });
+  } else if (payload === 'tourist') {
+    yelpClient.search({
+      term:'hiking',
+      location: 'Portland'
+    }).then(response => {
+      let firstB = response.jsonBody.businesses[0];
+      let secB = response.jsonBody.businesses[1];
+      let thirdB = response.jsonBody.businesses[2];
+      let msg = "The top hiking spots in the city of Portland are: " + firstB.name + " (" + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
+                                                                             + secB.name + " (" + secB.rating + " stars with " + secB.review_count + " reviews), and " 
+                                                                             + thirdB.name + " (" + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
+      
+      responseMessenger = produceResponseTemplate(firstB, secB, thirdB, msg);
+      callSendAPI(sender_psid, responseMessenger);  
+    }).catch(e => {
+      console.log(e);
+    });
+  } else if (payload === 'hotel') {
+    yelpClient.search({
+      term:'hotel',
+      location: 'Las Vegas'
+    }).then(response => {
+      let firstB = response.jsonBody.businesses[0];
+      let secB = response.jsonBody.businesses[1];
+      let thirdB = response.jsonBody.businesses[2];
+      let msg = "The top hotels in the city of Las Vegas are: " + firstB.name + " (" + firstB.price + ", " + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
+                                                                             + secB.name + " (" + secB.price + ", " + secB.rating + " stars with " + secB.review_count + " reviews), and " 
+                                                                             + thirdB.name + " (" + thirdB.price + ", " + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
+      
+      responseMessenger = produceResponseTemplate(firstB, secB, thirdB, msg);
+      callSendAPI(sender_psid, responseMessenger);  
+    }).catch(e => {
+      console.log(e);
+    });
+  } 
+}
+
+function produceResponseTemplate(firstB, secB, thirdB, msg) {
+  return {
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": msg,
+          "buttons": [
+			        {
+                "type":"web_url",
+                "url":firstB.url,
+                "title":firstB.name,
+              },
+              {
+                "type":"web_url",
+                "url":secB.url,
+                "title":secB.name,
+              },
+              {
+                "type":"web_url",
+                "url":thirdB.url,
+                "title":thirdB.name,
+              }
+            ]
+          }
+        }
+      };
 }
 
 // Sends response messages via the Send API
@@ -184,7 +260,7 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
   
-  //console.log(request_body);
+  console.log(request_body);
 
   // Send the HTTP request to the Messenger Platform
   request({
@@ -202,20 +278,21 @@ function callSendAPI(sender_psid, response) {
 }
 
 app.post('/ai', (req, res) => {
-  console.log('*** Webhook for yelp api query ***');
-  console.log(req.body.result);
-
   if (req.body.result.action === 'food') {
     console.log('*** food ***');
     let city = req.body.result.parameters['geo-city'];
-    
-    console.log("about to make yelp api request, city: ", city);
     
     yelpClient.search({
       term:'food',
       location: city
     }).then(response => {
-      let msg = "The top restaurants in the city of " + city + " are: " + response.jsonBody.businesses[0].name + ", " + response.jsonBody.businesses[1].name + ", and " + response.jsonBody.businesses[2].name + ".";
+      let firstB = response.jsonBody.businesses[0];
+      let secB = response.jsonBody.businesses[1];
+      let thirdB = response.jsonBody.businesses[2];
+      let msg = "The top restaurants in the city of " + city + " are: " + firstB.name + " (" + firstB.price + ", " + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
+                                                                                         + secB.name + " (" + secB.price + ", " + secB.rating + " stars with " + secB.review_count + " reviews), and " 
+                                                                                         + thirdB.name + " (" + thirdB.price + ", " + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
+                        
       return res.json({
           speech: msg,
           displayText: msg,
@@ -236,8 +313,6 @@ app.post('/ai', (req, res) => {
     let city = req.body.result.parameters['geo-city'];
     let foodtype = req.body.result.parameters['food-type'];
     
-    console.log("about to make yelp api request, city: ", city, "foodtype: ", foodtype);
-    
     yelpClient.search({
       term: foodtype,
       location: city
@@ -248,31 +323,72 @@ app.post('/ai', (req, res) => {
       let msg = "The top " + foodtype + " restaurants in the city of " + city + " are: " + firstB.name + " (" + firstB.price + ", " + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
                                                                                          + secB.name + " (" + secB.price + ", " + secB.rating + " stars with " + secB.review_count + " reviews), and " 
                                                                                          + thirdB.name + " (" + thirdB.price + ", " + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
-      let out_msg = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "button",
-            "text": `Choose one`,
-            "buttons": [{
-              "type": "postback",
-              "title": "Yes ur amazing!",
-              "payload": "yes",
-            },
-            {
-              "type": "postback",
-              "title": "No u suk old man!",
-              "payload": "no",
-            }]
-          }
-        }
-      };
-      
+                                                                                         
       return res.json({
-          speech: msg,
-          displayText: msg,
-          data: {"facebook": out_msg},
-          source: 'foodtype'
+          "speech": msg,
+          "displayText": msg,
+          "source": 'foodtype'
+      });
+    }).catch(e => {
+      console.log(e);
+      let errorMessage = 'I failed to look up the city name / food type.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+    });
+  } else if (req.body.result.action === 'tourism') {
+    console.log('*** tourism ***');
+    let city = req.body.result.parameters['geo-city'];
+    let tourismtype = req.body.result.parameters['tourism-type'];
+    
+    yelpClient.search({
+      term: tourismtype,
+      location: city
+    }).then(response => {
+      let firstB = response.jsonBody.businesses[0];
+      let secB = response.jsonBody.businesses[1];
+      let thirdB = response.jsonBody.businesses[2];
+      let msg = "The top " + tourismtype + " spots in the city of " + city + " are: " + firstB.name + " (" + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
+                                                                                         + secB.name + " (" + secB.rating + " stars with " + secB.review_count + " reviews), and " 
+                                                                                         + thirdB.name + " (" + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
+                                                                                         
+      return res.json({
+          "speech": msg,
+          "displayText": msg,
+          "source": 'tourism'
+      });
+    }).catch(e => {
+      console.log(e);
+      let errorMessage = 'I failed to look up the city name / tourism type.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+    });
+  } else if (req.body.result.action === 'hotel') {
+    console.log('*** hotel ***');
+    let city = req.body.result.parameters['geo-city'];
+    
+    yelpClient.search({
+      term: "hotel",
+      location: city
+    }).then(response => {
+      let firstB = response.jsonBody.businesses[0];
+      let secB = response.jsonBody.businesses[1];
+      let thirdB = response.jsonBody.businesses[2];
+      let msg = "The top hotels in the city of " + city + " are: " + firstB.name + " (" + firstB.price + ", " + firstB.rating + " stars with " + firstB.review_count + " reviews), " 
+                                                                                         + secB.name + " (" + secB.price + ", " + secB.rating + " stars with " + secB.review_count + " reviews), and " 
+                                                                                         + thirdB.name + " (" + thirdB.price + ", " + thirdB.rating + " stars with " + thirdB.review_count + " reviews).";
+                                                                                         
+      return res.json({
+          "speech": msg,
+          "displayText": msg,
+          "source": 'hotel'
       });
     }).catch(e => {
       console.log(e);
@@ -284,5 +400,31 @@ app.post('/ai', (req, res) => {
           }
         });
     });
+  } else if (req.body.result.action === 'weather') {
+    console.log('*** weather ***');
+    let city = req.body.result.parameters['geo-city'];
+    let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_APIKEY+'&q='+city+"&units=imperial";
+
+    request.get(apiUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let weatherResponse = JSON.parse(body);
+        let msg =  "Today, the weather condition in " + weatherResponse.name + " are " + weatherResponse.weather[0].description + ", with a low of " + weatherResponse.main.temp_min + " ℉ and a high of " + weatherResponse.main.temp_max + " ℉. "
+                    + "The current temperature is " + weatherResponse.main.temp + "℉, current humidity is at " + weatherResponse.main.humidity + "%, and cloud cover is at " + weatherResponse.clouds.all + "%. Winds are blowing at " + weatherResponse.wind.speed + "mph.";
+        
+        return res.json({
+          speech: msg,
+          displayText: msg,
+          source: 'weather'
+        });
+      } else {
+        let errorMessage = 'I failed to look up the city name.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+      }
+    })
   }
 });
